0615c6d65e03ba9cb5364169c9e74f4f2a91554b
Sebastian Garrido <sebasgarcep@users.noreply.github.com>
2017-01-14 11:54:31 -0500

    Re-export Mime

--- src/lib.rs
+++ src/lib.rs
@@ -100,6 +100,7 @@ extern crate url;
 pub use hyper::client::IntoUrl;
 pub use hyper::Error as HyperError;
 pub use hyper::header;
+pub use hyper::mime;
 pub use hyper::method::Method;
 pub use hyper::status::StatusCode;
 pub use hyper::version::HttpVersion;
