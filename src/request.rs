c928733d5732356cb5e967312719de75acda7ad9
voider1 <wesleyey0408@gmail.com>
2017-06-08 20:29:16 +0200

    Placing the \n somewhere else

--- src/request.rs
+++ src/request.rs
@@ -348,8 +348,8 @@ impl MultipartRequestBuilder {
         if let Some(p) = self.params.clone() {
             for (name, value) in p {
                 write_bytes!(body, "\r\n--{}\r\n", boundary);
-                write_bytes!(body, "Content-Disposition: form-data; name=\"{}\"", name);
-                write_bytes!(body, "\n\r\n{}\r\n", value);
+                write_bytes!(body, "Content-Disposition: form-data; name=\"{}\"\n", name);
+                write_bytes!(body, "\r\n{}\r\n", value);
             }
         }
 
