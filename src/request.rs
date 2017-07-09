35359d7484e35e2b9a053455582e6572fc8facc3
e00E <vakevk@gmail.com>
2017-07-09 16:59:30 +0200

    Make comment about header clearer

--- src/request.rs
+++ src/request.rs
@@ -274,9 +274,8 @@ impl RequestBuilder {
     pub fn multipart(&mut self, multipart: MultipartRequest) -> ::Result<&mut RequestBuilder> {
         {
             let mut req = self.request_mut();
-            // TODO: I tried to define the mimetype in code only, without parse() but
-            // could not get it work
-            // I think it should be possible but I dont know how
+            // TODO: I tried to define the mimetype in code only, without parse() but could not
+            // find a way to set the boundary parameter. Is there a way to do that?
             req.headers_mut().set(::header::ContentType(format!("multipart/form-data; boundary={}", multipart.boundary).parse().unwrap()));
             let reader = multipart.reader();
             *req.body_mut() = Some(Body::new(reader));
