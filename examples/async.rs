88ebb5db7fc002e7ef5a38db564b20fa33c90342
king6cong <king6cong@gmail.com>
2017-07-20 16:50:14 +0800

    add support of getting resolved body in async client

--- examples/async.rs
+++ examples/async.rs
@@ -1,16 +1,26 @@
 extern crate futures;
 extern crate reqwest;
 extern crate tokio_core;
+extern crate hyper;
 
 use futures::Future;
 
 fn main() {
     let mut core = tokio_core::reactor::Core::new().unwrap();
-    let client = reqwest::unstable::async::Client::new(&core.handle()).unwrap();
 
-    let work = client.get("https://hyper.rs").unwrap().send().map(|res| {
-        println!("{}", res.status());
-    });
+    let client = reqwest::unstable::async::ClientBuilder::new().unwrap()
+        .build(&core.handle()).unwrap();
+
+    let work = client.get("https://hyper.rs").unwrap().send().and_then(|mut res| {
+        let status = res.status();
+        let headers = res.headers().clone();
+        println!("status: {:?}", status);
+        println!("headers: {:?}", headers);
+        res.body_resolved()
+    })
+        .map(|resp| {
+            println!("resp: {:?}", String::from_utf8_lossy(&*resp));
+        });
 
     core.run(work).unwrap();
 }
