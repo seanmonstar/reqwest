a5ba16261ac34ed54b94b5ae4e44f3f8842f5cee
Scott Schroeder <scottschroeder@sent.com>
2017-09-02 16:15:33 -0700

    Add option to allow connections with invalid ssl certificates

--- src/async_impl/client.rs
+++ src/async_impl/client.rs
@@ -123,6 +123,21 @@ impl ClientBuilder {
         self
     }
 
+    /// Specifies whether to trust invalid certificates.
+    ///
+    /// # Warning
+    ///
+    /// You should think very carefully before using this method. If invalid
+    /// certificates are trusted, *any* certificate for *any* site will be
+    /// trusted for use. This includes expired certificates. This introduces
+    /// significant vulnerabilities, and should only be used as a last resort.
+    pub fn danger_disable_certificate_validation_entirely(&mut self) -> &mut ClientBuilder {
+        if let Some(config) = config_mut(&mut self.config, &self.err) {
+            config.tls.danger_disable_certificate_validation_entirely();
+        }
+        self
+    }
+
     /// Sets the identity to be used for client certificate authentication.
     pub fn identity(&mut self, identity: Identity) -> &mut ClientBuilder {
         if let Some(config) = config_mut(&mut self.config, &self.err) {
