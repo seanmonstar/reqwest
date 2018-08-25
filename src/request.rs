c8e996bc8b12a05936efade432c9fbb5202d57cf
Andrey Kutejko <andy128k@gmail.com>
2018-08-25 13:30:22 +0200

    Update request.rs

--- src/request.rs
+++ src/request.rs
@@ -220,7 +220,7 @@ impl RequestBuilder {
             Some(password) => format!("{}:{}", username, password),
             None => format!("{}:", username)
         };
-        let header_value = format!("basic {}", encode(&auth));
+        let header_value = format!("Basic {}", encode(&auth));
         self.header(::header::AUTHORIZATION, &*header_value)
     }
 
@@ -239,7 +239,7 @@ impl RequestBuilder {
     where
         T: fmt::Display,
     {
-        let header_value = format!("bearer {}", token);
+        let header_value = format!("Bearer {}", token);
         self.header(::header::AUTHORIZATION, &*header_value)
     }
 
