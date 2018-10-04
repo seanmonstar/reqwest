078e35d4430dd4732ce8a0d66821564d4f8aab8a
Paul Woolcock <pwoolcock@tenable.com>
2018-10-04 14:20:42 -0400

    Add `From<http::Response> for async_impl::Response`
    This adds an implementation to convert a `Response` type from the `http`
    crate to the `async_impl::Response` type. This is the first step to
    allow us to convert `http::Response` objects to `request::Response`
    objects
    
    This also adds an extension trait for the `http::response::Builder`
    type. The `http::Response` object does not provide a way to access the
    "final" url that the response is derived from, so we can't easily
    provide that in the `From<http::Response>` implementation. For users who
    are manually constructing `http::Response` objects for use in tests,
    etc, they can import this extension trait, which adds a `.url()` builder
    method that will allow them to pass a `Url`, which we then convert to
    our newtype'd Url and add to the `http::Response`'s `extensions`. Then,
    when converting from `http::Response` to `async_impl::Response` we can
    pull that value out of the `extensions` and use it to construct the
    `async_impl::Response`

--- src/async_impl/mod.rs
+++ src/async_impl/mod.rs
@@ -2,7 +2,7 @@ pub use self::body::{Body, Chunk};
 pub use self::decoder::{Decoder, ReadableChunks};
 pub use self::client::{Client, ClientBuilder};
 pub use self::request::{Request, RequestBuilder};
-pub use self::response::Response;
+pub use self::response::{Response, ResponseBuilderExt};
 
 pub mod body;
 pub mod client;
