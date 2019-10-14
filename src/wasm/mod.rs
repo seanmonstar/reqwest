5e5e6e90406cc3a23b068dcec303c5f3575cde43
lzutao <taolzu@gmail.com>
2019-10-15 01:25:17 +0700

    chore(deps): bump wasm-bindgen-futures (#675)

--- src/wasm/mod.rs
+++ src/wasm/mod.rs
@@ -15,7 +15,7 @@ async fn promise<T>(promise: js_sys::Promise) -> Result<T, crate::error::BoxErro
 where
     T: JsCast,
 {
-    use wasm_bindgen_futures::futures_0_3::JsFuture;
+    use wasm_bindgen_futures::JsFuture;
 
     let js_val = JsFuture::from(promise)
         .await
