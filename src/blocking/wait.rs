8b65556ac9a5978e6cfc43c10894f18ec6e08bdc
Sean McArthur <sean@seanmonstar.com>
2019-12-10 14:13:05 -0800

    replace tokio-executor's ParkThread with vendored ThreadWaker

--- src/blocking/wait.rs
+++ src/blocking/wait.rs
@@ -1,29 +1,28 @@
 use std::future::Future;
 use std::sync::Arc;
 use std::task::{Context, Poll};
+use std::thread::{self, Thread};
 use std::time::Duration;
 
 use tokio::time::Instant;
-use tokio_executor::{
-    enter,
-    park::{Park, ParkThread, Unpark, UnparkThread},
-};
 
 pub(crate) fn timeout<F, I, E>(fut: F, timeout: Option<Duration>) -> Result<I, Waited<E>>
 where
     F: Future<Output = Result<I, E>>,
 {
+    /*
     let _entered =
         enter().map_err(|_| Waited::Executor(crate::error::BlockingClientInAsyncContext))?;
+    */
     let deadline = timeout.map(|d| {
         log::trace!("wait at most {:?}", d);
         Instant::now() + d
     });
 
-    let mut park = ParkThread::new();
-    // Arc shouldn't be necessary, since UnparkThread is reference counted internally,
+    let thread = ThreadWaker(thread::current());
+    // Arc shouldn't be necessary, since `Thread` is reference counted internally,
     // but let's just stay safe for now.
-    let waker = futures_util::task::waker(Arc::new(UnparkWaker(park.unpark())));
+    let waker = futures_util::task::waker(Arc::new(thread));
     let mut cx = Context::from_waker(&waker);
 
     futures_util::pin_mut!(fut);
@@ -43,10 +42,9 @@ where
             }
 
             log::trace!("park timeout {:?}", deadline - now);
-            park.park_timeout(deadline - now)
-                .expect("ParkThread doesn't error");
+            thread::park_timeout(deadline - now);
         } else {
-            park.park().expect("ParkThread doesn't error");
+            thread::park();
         }
     }
 }
@@ -58,9 +56,9 @@ pub(crate) enum Waited<E> {
     Inner(E),
 }
 
-struct UnparkWaker(UnparkThread);
+struct ThreadWaker(Thread);
 
-impl futures_util::task::ArcWake for UnparkWaker {
+impl futures_util::task::ArcWake for ThreadWaker {
     fn wake_by_ref(arc_self: &Arc<Self>) {
         arc_self.0.unpark();
     }
