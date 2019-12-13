34d482d7463d7bd3291a8bf9fc94d26c3cb5d1ef
Constantin Nickel <constantin.nickel@gmail.com>
2019-12-13 03:40:18 +0100

    Export the `ResponseBuilderExt` trait again

--- src/lib.rs
+++ src/lib.rs
@@ -276,7 +276,7 @@ if_hyper! {
     doctest!("../README.md");
 
     pub use self::async_impl::{
-        multipart, Body, Client, ClientBuilder, Request, RequestBuilder, Response,
+        multipart, Body, Client, ClientBuilder, Request, RequestBuilder, Response, ResponseBuilderExt,
     };
     pub use self::proxy::Proxy;
     pub use self::redirect::{RedirectAction, RedirectAttempt, RedirectPolicy};
