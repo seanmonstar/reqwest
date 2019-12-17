79a49cd89d31aece2dcb7b369aba58cd5f7cd7bb
Sean McArthur <sean@seanmonstar.com>
2019-12-17 12:27:43 -0800

    Add note about how to use tokio::main in examples

--- examples/simple.rs
+++ examples/simple.rs
@@ -1,11 +1,11 @@
 #![deny(warnings)]
 
+// This is using the `tokio` runtime. You'll need the following dependency:
+//
+// `tokio = { version = "0.2", features = ["macros"] }`
 #[tokio::main]
 async fn main() -> Result<(), reqwest::Error> {
-    let res = reqwest::Client::new()
-        .get("https://hyper.rs")
-        .send()
-        .await?;
+    let res = reqwest::get("https://hyper.rs").await?;
 
     println!("Status: {}", res.status());
 
