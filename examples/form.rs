661ecc211379676b0da971c874b5ea55d4c6dd69
steve <dev@somecool.net>
2020-11-15 02:32:24 +0000

    fixed typo

--- examples/form.rs
+++ examples/form.rs
@@ -4,7 +4,7 @@
 #[tokio::main]
 async fn main() {
     let response = reqwest::Client::new()
-        .post("http://www.baidu.xyzcom")
+        .post("http://www.baidu.com")
         .form(&[("one", "1")])
         .send()
         .await
