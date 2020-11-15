a98160e2092441b2c44f49ca28c5eb59cf041cf8
steve <dev@somecool.net>
2020-11-15 02:26:29 +0000

    add comment describing the need for cfg switches for wasm32 builds

--- examples/simple.rs
+++ examples/simple.rs
@@ -17,5 +17,9 @@ async fn main() -> Result<(), reqwest::Error> {
     Ok(())
 }
 
+// The [cfg(not(target_arch = "wasm32"))] above prevent building the tokio::main function
+// for wasm32 target, because tokio isn't compatible with wasm32.
+// If you aren't building for wasm32, you don't need that line.
+// The two lines below avoid the "'main' function not found" error when building for wasm32 target.
 #[cfg(target_arch = "wasm32")]
 fn main() {}
