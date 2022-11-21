9d74ad9734ded1c978d70804ec34a8e7c11d4e48
Valentin Bersier <vbersier@gmail.com>
2022-11-21 16:51:25 +0100

    feat(async_impl): set custom headers on multipart Part

--- src/async_impl/multipart.rs
+++ src/async_impl/multipart.rs
@@ -244,6 +244,14 @@ impl Part {
         self.with_inner(move |inner| inner.file_name(filename))
     }
 
+    /// Sets custom headers for the part.
+    pub fn headers<T>(self, headers: T) -> Part
+    where
+        T: Into<HeaderMap>,
+    {
+        self.with_inner(move |inner| inner.headers(headers))
+    }
+
     fn with_inner<F>(self, func: F) -> Self
     where
         F: FnOnce(PartMetadata) -> PartMetadata,
@@ -392,6 +400,14 @@ impl PartMetadata {
         T: Into<Cow<'static, str>>,
     {
         self.file_name = Some(filename.into());
+        self
+    }
+
+    pub(crate) fn headers<T>(mut self, headers: T) -> Self
+    where
+        T: Into<HeaderMap>,
+    {
+        self.headers = headers.into();
         self
     }
 }
