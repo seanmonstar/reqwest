09513995d4a7304e54ee14b85d2b5d8aa339c224
link2xt <link2xt@testrun.org>
2023-01-01 23:41:45 +0000

    Documentation fix: it's `TCP_NODELAY`, not `SO_NODELAY`

--- src/async_impl/client.rs
+++ src/async_impl/client.rs
@@ -1000,7 +1000,7 @@ impl ClientBuilder {
 
     // TCP options
 
-    /// Set whether sockets have `SO_NODELAY` enabled.
+    /// Set whether sockets have `TCP_NODELAY` enabled.
     ///
     /// Default is `true`.
     pub fn tcp_nodelay(mut self, enabled: bool) -> ClientBuilder {
