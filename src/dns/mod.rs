5e033c10c4cca73e907fdc20cf0753537d834a59
Lakshmikanth Devaraju <84574863+LakhsmikanthD@users.noreply.github.com>
2023-09-04 16:31:21 +0530

    Update mod.rs

--- src/dns/mod.rs
+++ src/dns/mod.rs
@@ -6,4 +6,4 @@ pub(crate) use resolve::{DnsResolverWithOverrides, DynResolver};
 pub(crate) mod gai;
 pub(crate) mod resolve;
 #[cfg(feature = "trust-dns")]
-pub(crate) mod trust_dns;
+pub mod trust_dns;
