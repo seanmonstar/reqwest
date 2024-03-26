54e09bc045b4d313f1e0287a11f0e6163b981a97
zuisong <com.me@foxmail.com>
2024-03-26 21:42:38 +0800

    add test case

--- src/async_impl/body.rs
+++ src/async_impl/body.rs
@@ -355,6 +355,8 @@ where
 
 #[cfg(test)]
 mod tests {
+    use http_body::Body as _;
+
     use super::Body;
 
     #[test]
@@ -362,5 +364,20 @@ mod tests {
         let test_data = b"Test body";
         let body = Body::from(&test_data[..]);
         assert_eq!(body.as_bytes(), Some(&test_data[..]));
+    }
+
+    #[test]
+    fn body_exact_length() {
+        let empty_body = Body::empty();
+        assert!(empty_body.is_end_stream());
+        assert_eq!(empty_body.size_hint().exact(), Some(0));
+
+        let bytes_body = Body::reusable("abc".into());
+        assert!(!bytes_body.is_end_stream());
+        assert_eq!(bytes_body.size_hint().exact(), Some(3));
+
+        let stream_body = Body::streaming(bytes_body);
+        assert!(!stream_body.is_end_stream());
+        assert_eq!(stream_body.size_hint().exact(), None);
     }
 }
--- tests/blocking.rs
+++ tests/blocking.rs
@@ -1,7 +1,6 @@
 mod support;
 
-#[cfg(feature = "json")]
-use http::header::CONTENT_TYPE;
+use http::header::{CONTENT_LENGTH, CONTENT_TYPE, TRANSFER_ENCODING};
 use http_body_util::BodyExt;
 #[cfg(feature = "json")]
 use std::collections::HashMap;
@@ -19,6 +18,29 @@ fn test_response_text() {
 
     let body = res.text().unwrap();
     assert_eq!(b"Hello", body.as_bytes());
+}
+
+#[test]
+fn donot_set_conent_length_0_if_have_no_body() {
+    let server = server::http(move |req| async move {
+        let headers = req.headers();
+        assert_eq!(headers.get(CONTENT_LENGTH), None);
+        assert!(headers.get(CONTENT_TYPE).is_none());
+        assert!(headers.get(TRANSFER_ENCODING).is_none());
+        dbg!(&headers);
+        http::Response::default()
+    });
+
+    let url = format!("http://{}/conent-length", server.addr());
+    let res = reqwest::blocking::Client::builder()
+        .no_proxy()
+        .build()
+        .expect("client builder")
+        .post(&url)
+        .send()
+        .expect("request");
+
+    assert_eq!(res.status(), reqwest::StatusCode::OK);
 }
 
 #[test]
