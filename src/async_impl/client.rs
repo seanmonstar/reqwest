50b3e714797874b4e8df9ebb4de977fbcd27b510
josephlenton@gmail.com <josephlenton@gmail.com>
2024-08-20 11:59:39 +0200

    feat: add base_url parsing

--- src/async_impl/client.rs
+++ src/async_impl/client.rs
@@ -224,9 +224,9 @@ struct Config {
     h3_send_grease: Option<bool>,
     dns_overrides: HashMap<String, Vec<SocketAddr>>,
     dns_resolver: Option<Arc<dyn Resolve>>,
-
     #[cfg(unix)]
     unix_socket: Option<Arc<std::path::Path>>,
+    base_url: Option<Url>,
 }
 
 impl Default for ClientBuilder {
@@ -352,6 +352,7 @@ impl ClientBuilder {
                 dns_resolver: None,
                 #[cfg(unix)]
                 unix_socket: None,
+                base_url: None,
             },
         }
     }
@@ -1007,6 +1008,7 @@ impl ClientBuilder {
                 proxies_maybe_http_custom_headers,
                 https_only: config.https_only,
                 redirect_policy_desc,
+                base_url: config.base_url,
             }),
         })
     }
@@ -2442,7 +2444,27 @@ impl Client {
     ///
     /// This method fails whenever the supplied `Url` cannot be parsed.
     pub fn request<U: IntoUrl>(&self, method: Method, url: U) -> RequestBuilder {
-        let req = url.into_url().map(move |url| Request::new(method, url));
+        let mut maybe_url = url.into_url();
+
+        if let Some(base_url) = &self.inner.base_url {
+            if let Err(url_parse_error) = &maybe_url {
+                if url_parse_error.is_builder() {
+                    if let Some(url) = url_parse_error.url() {
+                        if !url.has_host() {
+                            let mut replacement_url = base_url.clone();
+
+                            replacement_url.set_path(url.path());
+                            replacement_url.set_query(url.query());
+                            replacement_url.set_fragment(url.fragment());
+
+                            maybe_url = Ok(replacement_url);
+                        }
+                    }
+                }
+            }
+        }
+
+        let req = maybe_url.map(move |url| Request::new(method, url));
         RequestBuilder::new(self.clone(), req)
     }
 
@@ -2801,6 +2823,7 @@ struct ClientRef {
     proxies_maybe_http_custom_headers: bool,
     https_only: bool,
     redirect_policy_desc: Option<String>,
+    base_url: Option<Url>,
 }
 
 impl ClientRef {
