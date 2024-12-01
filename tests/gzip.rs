266bbdbbf821b499655a9a8dc00fc546b47ea218
Andrew <averban02@mail.ru>
2024-12-01 23:56:02 +0100

    tests refactor

--- tests/gzip.rs
+++ tests/gzip.rs
@@ -152,45 +152,51 @@ async fn gzip_case(response_size: usize, chunk_size: usize) {
     assert_eq!(body, content);
 }
 
+const COMPRESSED_RESPONSE_HEADERS: &[u8] = b"HTTP/1.1 200 OK\x0d\x0a\
+            Content-Type: text/plain\x0d\x0a\
+            Connection: keep-alive\x0d\x0a\
+            Content-Encoding: gzip\x0d\x0a";
+
+const RESPONSE_CONTENT: &str = "some message here";
+
+fn gzip_compress(input: &[u8]) -> Vec<u8> {
+    let mut encoder = libflate::gzip::Encoder::new(Vec::new()).unwrap();
+    match encoder.write(input) {
+        Ok(n) => assert!(n > 0, "Failed to write to encoder."),
+        _ => panic!("Failed to gzip encode string."),
+    };
+    encoder.finish().into_result().unwrap()
+}
+
 #[tokio::test]
 async fn test_non_chunked_non_fragmented_response() {
     let server = server::low_level_with_response(|_raw_request, client_socket| {
         Box::new(async move {
-            let response = b"HTTP/1.1 200 OK\x0d\x0a\
-            Content-Type: text/plain\x0d\x0a\
-            Connection: keep-alive\x0d\x0a\
-            Content-Encoding: gzip\x0d\x0a\
-            Content-Length: 85\x0d\x0a\
-            \x0d\x0a\
-            \x1f\x8b\x08\x00\x00\x00\x00\x00\x00\x03\xabV*\xae\xccM\xca\xcfQ\xb2Rr\x0aq\x0e\x0dv\x09Q\xd2Q\xca/H\xcd\xf3\xcc+I-J-.\x01J\x98\x1b\x18\x98\x9a\xe9\x99\x9a\x18\x03\xa5J2sS\x95\xac\x0c\xcd\x8d\x8cM\x8cLML\x0c---j\x01\xd7Gb;D\x00\x00\x00";
+            let gzipped_content = gzip_compress(RESPONSE_CONTENT.as_bytes());
+            let content_length_header =
+                format!("Content-Length: {}\r\n\r\n", gzipped_content.len()).into_bytes();
+            let response = [
+                COMPRESSED_RESPONSE_HEADERS,
+                &content_length_header,
+                &gzipped_content,
+            ]
+            .concat();
 
             client_socket
-                .write_all(response)
+                .write_all(response.as_slice())
                 .await
                 .expect("response write_all failed");
             client_socket.flush().await.expect("response flush failed");
         })
     });
 
-    let client = reqwest::Client::builder()
-        .connection_verbose(true)
-        .timeout(Duration::from_secs(15))
-        .pool_idle_timeout(Some(std::time::Duration::from_secs(300)))
-        .pool_max_idle_per_host(5)
-        .build()
-        .expect("reqwest client init error");
-
-    let res = client
+    let res = reqwest::Client::new()
         .get(&format!("http://{}/", server.addr()))
         .send()
         .await
         .expect("response");
 
-    let body = res.text().await.expect("text");
-    assert_eq!(
-        body,
-        r#"{"symbol":"BTCUSDT","openInterest":"70056.543","time":1723425441998}"#
-    );
+    assert_eq!(res.text().await.expect("text"), RESPONSE_CONTENT);
 }
 
 #[tokio::test]
@@ -201,18 +207,21 @@ async fn test_chunked_fragmented_response_1() {
 
     let server = server::low_level_with_response(|_raw_request, client_socket| {
         Box::new(async move {
-            let response_first_part = b"HTTP/1.1 200 OK\x0d\x0a\
-            Content-Type: text/plain\x0d\x0a\
-            Transfer-Encoding: chunked\x0d\x0a\
-            Connection: keep-alive\x0d\x0a\
-            Content-Encoding: gzip\x0d\x0a\
-            \x0d\x0a\
-            55\x0d\x0a\
-            \x1f\x8b\x08\x00\x00\x00\x00\x00\x00\x03\xabV*\xae\xccM\xca\xcfQ\xb2Rr\x0aq\x0e\x0dv\x09Q\xd2Q\xca/H\xcd\xf3\xcc+I-J-.\x01J\x98\x1b\x18\x98\x9a\xe9\x99\x9a\x18\x03\xa5J2sS\x95\xac\x0c\xcd\x8d\x8cM\x8cLML\x0c---j\x01\xd7Gb;D\x00\x00\x00";
-            let response_second_part = b"\x0d\x0a0\x0d\x0a\x0d\x0a";
+            let gzipped_content = gzip_compress(RESPONSE_CONTENT.as_bytes());
+            let response_first_part = [
+                COMPRESSED_RESPONSE_HEADERS,
+                format!(
+                    "Transfer-Encoding: chunked\r\n\r\n{:x}\r\n",
+                    gzipped_content.len()
+                )
+                .as_bytes(),
+                &gzipped_content,
+            ]
+            .concat();
+            let response_second_part = b"\r\n0\r\n\r\n";
 
             client_socket
-                .write_all(response_first_part)
+                .write_all(response_first_part.as_slice())
                 .await
                 .expect("response_first_part write_all failed");
             client_socket
@@ -234,26 +243,13 @@ async fn test_chunked_fragmented_response_1() {
     });
 
     let start = tokio::time::Instant::now();
-
-    let client = reqwest::Client::builder()
-        .connection_verbose(true)
-        .timeout(Duration::from_secs(15))
-        .pool_idle_timeout(Some(std::time::Duration::from_secs(300)))
-        .pool_max_idle_per_host(5)
-        .build()
-        .expect("reqwest client init error");
-
-    let res = client
+    let res = reqwest::Client::new()
         .get(&format!("http://{}/", server.addr()))
         .send()
         .await
         .expect("response");
 
-    let body = res.text().await.expect("text");
-    assert_eq!(
-        body,
-        r#"{"symbol":"BTCUSDT","openInterest":"70056.543","time":1723425441998}"#
-    );
+    assert_eq!(res.text().await.expect("text"), RESPONSE_CONTENT);
     assert!(start.elapsed() >= DELAY_BETWEEN_RESPONSE_PARTS - DELAY_MARGIN);
 }
 
@@ -265,18 +261,22 @@ async fn test_chunked_fragmented_response_2() {
 
     let server = server::low_level_with_response(|_raw_request, client_socket| {
         Box::new(async move {
-            let response_first_part = b"HTTP/1.1 200 OK\x0d\x0a\
-            Content-Type: text/plain\x0d\x0a\
-            Transfer-Encoding: chunked\x0d\x0a\
-            Connection: keep-alive\x0d\x0a\
-            Content-Encoding: gzip\x0d\x0a\
-            \x0d\x0a\
-            55\x0d\x0a\
-            \x1f\x8b\x08\x00\x00\x00\x00\x00\x00\x03\xabV*\xae\xccM\xca\xcfQ\xb2Rr\x0aq\x0e\x0dv\x09Q\xd2Q\xca/H\xcd\xf3\xcc+I-J-.\x01J\x98\x1b\x18\x98\x9a\xe9\x99\x9a\x18\x03\xa5J2sS\x95\xac\x0c\xcd\x8d\x8cM\x8cLML\x0c---j\x01\xd7Gb;D\x00\x00\x00\x0d\x0a";
-            let response_second_part = b"0\x0d\x0a\x0d\x0a";
+            let gzipped_content = gzip_compress(RESPONSE_CONTENT.as_bytes());
+            let response_first_part = [
+                COMPRESSED_RESPONSE_HEADERS,
+                format!(
+                    "Transfer-Encoding: chunked\r\n\r\n{:x}\r\n",
+                    gzipped_content.len()
+                )
+                .as_bytes(),
+                &gzipped_content,
+                b"\r\n"
+            ]
+            .concat();
+            let response_second_part = b"0\r\n\r\n";
 
             client_socket
-                .write_all(response_first_part)
+                .write_all(response_first_part.as_slice())
                 .await
                 .expect("response_first_part write_all failed");
             client_socket
@@ -298,26 +298,13 @@ async fn test_chunked_fragmented_response_2() {
     });
 
     let start = tokio::time::Instant::now();
-
-    let client = reqwest::Client::builder()
-        .connection_verbose(true)
-        .timeout(Duration::from_secs(15))
-        .pool_idle_timeout(Some(std::time::Duration::from_secs(300)))
-        .pool_max_idle_per_host(5)
-        .build()
-        .expect("reqwest client init error");
-
-    let res = client
+    let res = reqwest::Client::new()
         .get(&format!("http://{}/", server.addr()))
         .send()
         .await
         .expect("response");
 
-    let body = res.text().await.expect("text");
-    assert_eq!(
-        body,
-        r#"{"symbol":"BTCUSDT","openInterest":"70056.543","time":1723425441998}"#
-    );
+    assert_eq!(res.text().await.expect("text"), RESPONSE_CONTENT);
     assert!(start.elapsed() >= DELAY_BETWEEN_RESPONSE_PARTS - DELAY_MARGIN);
 }
 
@@ -329,18 +316,21 @@ async fn test_chunked_fragmented_response_with_extra_bytes() {
 
     let server = server::low_level_with_response(|_raw_request, client_socket| {
         Box::new(async move {
-            let response_first_part = b"HTTP/1.1 200 OK\x0d\x0a\
-            Content-Type: text/plain\x0d\x0a\
-            Transfer-Encoding: chunked\x0d\x0a\
-            Connection: keep-alive\x0d\x0a\
-            Content-Encoding: gzip\x0d\x0a\
-            \x0d\x0a\
-            55\x0d\x0a\
-            \x1f\x8b\x08\x00\x00\x00\x00\x00\x00\x03\xabV*\xae\xccM\xca\xcfQ\xb2Rr\x0aq\x0e\x0dv\x09Q\xd2Q\xca/H\xcd\xf3\xcc+I-J-.\x01J\x98\x1b\x18\x98\x9a\xe9\x99\x9a\x18\x03\xa5J2sS\x95\xac\x0c\xcd\x8d\x8cM\x8cLML\x0c---j\x01\xd7Gb;D\x00\x00\x00";
-            let response_second_part = b"\x0d\x0a2ab\x0d\x0a0\x0d\x0a\x0d\x0a";
+            let gzipped_content = gzip_compress(RESPONSE_CONTENT.as_bytes());
+            let response_first_part = [
+                COMPRESSED_RESPONSE_HEADERS,
+                format!(
+                    "Transfer-Encoding: chunked\r\n\r\n{:x}\r\n",
+                    gzipped_content.len()
+                )
+                .as_bytes(),
+                &gzipped_content,
+            ]
+            .concat();
+            let response_second_part = b"\r\n2ab\r\n0\r\n\r\n";
 
             client_socket
-                .write_all(response_first_part)
+                .write_all(response_first_part.as_slice())
                 .await
                 .expect("response_first_part write_all failed");
             client_socket
@@ -362,16 +352,7 @@ async fn test_chunked_fragmented_response_with_extra_bytes() {
     });
 
     let start = tokio::time::Instant::now();
-
-    let client = reqwest::Client::builder()
-        .connection_verbose(true)
-        .timeout(Duration::from_secs(15))
-        .pool_idle_timeout(Some(std::time::Duration::from_secs(300)))
-        .pool_max_idle_per_host(5)
-        .build()
-        .expect("reqwest client init error");
-
-    let res = client
+    let res = reqwest::Client::new()
         .get(&format!("http://{}/", server.addr()))
         .send()
         .await
