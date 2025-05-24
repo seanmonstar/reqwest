4249e918c20e309218c7d2b6ae876c011a4be15f
Lori-Shu <2412045439@qq.com>
2025-05-24 10:01:03 +0800

    add a BLOCKING_CLIENT_CONTEXT variable to save current status of blocking clients, async client build() will panic if there are blocking clients exist

--- src/blocking/mod.rs
+++ src/blocking/mod.rs
@@ -72,7 +72,7 @@ mod response;
 mod wait;
 
 pub use self::body::Body;
-pub use self::client::{Client, ClientBuilder};
+pub use self::client::{Client, ClientBuilder,BLOCKING_CLIENT_CONTEXT,ClientType};
 pub use self::request::{Request, RequestBuilder};
 pub use self::response::Response;
 
