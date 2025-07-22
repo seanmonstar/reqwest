5a3d53f5a32021931c1fce22843162c3c27b579a
Kevin Ottens <ervin@ipsquad.net>
2025-07-23 01:45:42 +0200

    Expose the default headers used by clients
    This is convenient in debug and testing scenario for code using
    reqwest. In particular for introspecting to know what will be sent
    without introducing a MITM.

--- src/blocking/client.rs
+++ src/blocking/client.rs
@@ -1226,6 +1226,11 @@ impl Client {
         ClientBuilder::new()
     }
 
+    /// Get the headers this `Client` will add to all its `Request`s.
+    pub fn headers(&self) -> &crate::header::HeaderMap {
+        &self.inner.headers
+    }
+
     /// Convenience method to make a `GET` request to a URL.
     ///
     /// # Errors
@@ -1329,6 +1334,7 @@ impl fmt::Debug for ClientBuilder {
 #[derive(Clone)]
 struct ClientHandle {
     timeout: Timeout,
+    headers: crate::header::HeaderMap,
     inner: Arc<InnerClientHandle>,
 }
 
@@ -1359,6 +1365,7 @@ impl Drop for InnerClientHandle {
 impl ClientHandle {
     fn new(builder: ClientBuilder) -> crate::Result<ClientHandle> {
         let timeout = builder.timeout;
+        let headers = builder.inner.config.headers.clone();
         let builder = builder.inner;
         let (tx, rx) = mpsc::unbounded_channel::<(async_impl::Request, OneshotResponse)>();
         let (spawn_tx, spawn_rx) = oneshot::channel::<crate::Result<()>>();
@@ -1427,6 +1434,7 @@ impl ClientHandle {
 
         Ok(ClientHandle {
             timeout,
+            headers,
             inner: inner_handle,
         })
     }
